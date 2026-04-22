1073ad9cfb8f384011b85b87caf38d64fd13af1d
Tony3243 <113482331+Tony3243@users.noreply.github.com>
2026-04-22 11:47:53 -0700

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.js
@@ -0,0 +1,36 @@
+class MinStack {
+    constructor() {
+        this.stack = [] //creates our stack
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        //
+        const findMin = this.stack.length === 0 ? val : Math.min(val, this.stack[this.stack.length - 1][1])
+        this.stack.push([val, findMin])
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length - 1][0]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return this.stack[this.stack.length - 1][1]
+    }
+}
